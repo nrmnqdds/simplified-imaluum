70c56db6950c28f84b3103f2c0d2ffb8daa227f1
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-01 14:52:14 -0700

    :sparkles: Implements dark mode toggle

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "./theme-provider";
+import { ThemeSwitcher } from "../components/ThemeSwitcher";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeSwitcher />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ components/ThemeSwitcher.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { BsSun, BsMoon } from "react-icons/bs";
+import React from "react";
+
+export const ThemeSwitcher = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button
+      className={`w-fit absolute right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 text-zinc-900 dark:text-slate-200 text-2xl font-bold`}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "light" ? <BsMoon /> : <BsSun />}
+    </button>
+  );
+};
