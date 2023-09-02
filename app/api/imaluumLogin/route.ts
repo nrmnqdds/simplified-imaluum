9c3df47bada3ac5fe701e83f9dd36d0c1cd92d34
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-03 05:19:23 +0800

    :construction: Uses headless mode for puppeteer

--- app/api/imaluumLogin/route.ts
+++ app/api/imaluumLogin/route.ts
@@ -1,29 +1,44 @@
 import { NextResponse } from "next/server";
 import puppeteer from "puppeteer";
+import { IMALUUMLOGINPAGE } from "../../constants";
+// import chromium from "chrome-aws-lambda";
 
 export async function POST(request: Request) {
   const { username, password } = await request.json();
+  console.log("Launching browser");
 
   const browser = await puppeteer.launch({
     headless: true, // Set to true for production
     args: ["--no-sandbox"],
   });
 
+  // const browser = await chromium.puppeteer.launch({
+  //   args: chromium.args,
+  //   defaultViewport: chromium.defaultViewport,
+  //   executablePath: await chromium.executablePath,
+  //   headless: chromium.headless,
+  //   ignoreHTTPSErrors: true,
+  // });
+
   const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(0);
 
   try {
-    await page.goto(
-      "https://cas.iium.edu.my:8448/cas/login?service=https%3a%2f%2fimaluum.iium.edu.my%2fhome"
-    );
+    console.log("Opening page");
+    await page.goto(IMALUUMLOGINPAGE);
 
+    console.log("Typing username");
     await page.type("input#username", username);
     await new Promise((r) => setTimeout(r, 500));
+    console.log("Typing password");
     await page.type("input#password", password);
     await new Promise((r) => setTimeout(r, 500));
+    console.log("Clicking submit");
     await page.click("input.btn");
 
     await page.waitForNavigation();
 
+    console.log("Getting cookies");
     const cookies = await page.cookies();
 
     return NextResponse.json({ cookies });
