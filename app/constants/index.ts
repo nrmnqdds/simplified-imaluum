94f89d280480c1990c108442f9a75fc184c66a4f
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-04 18:38:01 +0800

    :bento: Add more constants

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,2 +1,4 @@
-export const IMALUUMLOGINPAGE =
+export const IMALUUM_LOGIN_PAGE =
   "https://cas.iium.edu.my:8448/cas/login?service=https%3a%2f%2fimaluum.iium.edu.my%2fhome";
+
+export const IMALUUM_HOME_PAGE = "https://imaluum.iium.edu.my/home";
