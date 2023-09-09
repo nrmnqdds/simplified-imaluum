8d88434ab4250fb7fc3f8029a225dc1ee9485535
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-09 17:50:12 +0800

    :wrench: Implements Server Actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverRuntimeConfig: {
+    experimental: {
+      serverActions: true,
+    },
+  },
+};
 
 module.exports = nextConfig;
