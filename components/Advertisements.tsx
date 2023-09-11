ff514e261fda6a8f65b9447cc5ab2b0c684cad45
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-11 16:03:23 +0800

    :lipstick: Adds skeleton loader

--- components/Advertisements.tsx
+++ components/Advertisements.tsx
@@ -3,12 +3,15 @@
 import { useState, useEffect, Fragment } from "react";
 import dynamic from "next/dynamic";
 import Link from "next/link";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import { Skeleton } from "@mui/material";
 
 const ScrollCarousel = dynamic(() => import("./ScrollCarouselComponent"), {
   ssr: false,
 });
 
-const Advertisement = () => {
+const Advertisement = ({ className }) => {
   const [ads, setAds] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
@@ -23,23 +26,40 @@ const Advertisement = () => {
   }, []);
 
   return (
-    <section className="relative self-end w-full h-[35%] flex flex-col">
+    <section className={className}>
       <div className="m-3 flex justify-between w-full items-center">
         <h2 className="text-2xl font-bold text-zinc-900 dark:text-slate-100">
+          <span className="mr-2">
+            <NewspaperIcon />
+          </span>
           SOUQ Advertisement
         </h2>
         <Link
           href="https://souq.iium.edu.my/list"
           target="_blank"
-          className="text-blue-500 underline"
+          className="text-blue-500 hover:text-blue-700"
         >
-          See More{" >"}
+          See More
+          <span>
+            <KeyboardDoubleArrowRightIcon />
+          </span>
         </Link>
       </div>
 
       <div className="flex flex-row gap-2 w-full h-full">
         {loading ? (
-          <p>Loading...</p>
+          // <p>Loading...</p>
+          <div className="flex flex-row gap-2 overflow-hidden">
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+              <Skeleton
+                key={item}
+                sx={{ bgcolor: "grey.900" }}
+                variant="rectangular"
+                width={144}
+                height={144}
+              />
+            ))}
+          </div>
         ) : (
           <Fragment>
             <ScrollCarousel ads={ads} />
