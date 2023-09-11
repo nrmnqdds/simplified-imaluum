83f3a86d04369feff0d9e825649bfb4d91544ec5
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-11 16:03:44 +0800

    :construction: Dummy component

---
+++ components/Disciplinary.tsx
@@ -0,0 +1,9 @@
+const Disciplinary = ({ className }) => {
+  return (
+    <div className={`flex items-center justify-center ${className}`}>
+      <h1>Disciplinary</h1>
+    </div>
+  );
+};
+
+export default Disciplinary;
