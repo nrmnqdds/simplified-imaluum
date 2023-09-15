32cd5211b0fb09aa272f1e327995e405ae501469
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-15 21:46:36 +0800

    :alien: Call schedule and session scraper api

--- components/TimeTable.tsx
+++ components/TimeTable.tsx
@@ -1,5 +1,104 @@
-import WeeklyCalendar from "./WeeklyCalendar";
+import { useEffect, useState, Fragment, useTransition } from "react";
+import { useCookiesProvider } from "../app/context/cookies-provider";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 export default function TimeTable() {
-  return <WeeklyCalendar />;
+  const [loading, setLoading] = useState(true);
+  const [isPending, startTransition] = useTransition();
+  const { cookies } = useCookiesProvider();
+
+  const [sessionsList, setSessionsList] = useState({ sessionList: [] });
+  const [session, setSession] = useState("");
+  const [schedule, setSchedule] = useState({
+    schedule: [
+      {
+        courseCode: "",
+        days: 0,
+        time: ["1130", "1250"],
+      },
+    ],
+  });
+
+  useEffect(() => {
+    async function getSession() {
+      const response = await fetch(`api/session`, {
+        headers: {
+          Cookie: cookies.toString(),
+        },
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        setSessionsList(responseData);
+        if (responseData.sessionList.length > 1) {
+          setSession(responseData.sessionList[1].sessionQuery);
+        }
+      } else {
+        console.error("Error fetching user data");
+      }
+    }
+    getSession();
+  }, [cookies]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`api/schedule`, {
+          method: "POST",
+          headers: {
+            Cookie: cookies.toString(),
+          },
+          body: JSON.stringify({ session }),
+        });
+
+        if (response.ok) {
+          const responseData = await response.json();
+          setSchedule(responseData);
+        } else {
+          console.error("Error fetching user data");
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (session) {
+      fetchData();
+    }
+  }, [cookies, session]);
+
+  const handleChange = (event: any) => {
+    startTransition(() => {
+      setSession(event.target.value);
+    });
+  };
+
+  return (
+    <Fragment>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Session</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={session}
+            label="Session"
+            onChange={handleChange}
+          >
+            {sessionsList.sessionList.map((session, index) => (
+              <MenuItem key={index} value={session.sessionQuery}>
+                {session.sessionName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+    </Fragment>
+  );
 }
