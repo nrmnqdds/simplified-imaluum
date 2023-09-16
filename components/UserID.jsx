a55761032e220451ac2261c4613d8e2dcb87d43f
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-16 15:13:53 +0800

    :fire: Removes schedule option

--- components/UserID.jsx
+++ components/UserID.jsx
@@ -53,10 +53,6 @@ const UserID = () => {
     setAnchorEl(null);
     router.replace("/");
   };
-  const handleSchedule = () => {
-    setAnchorEl(null);
-    router.replace("/dashboard/schedule");
-  };
 
   return loading ? (
     <Skeleton
@@ -111,7 +107,6 @@ const UserID = () => {
           "aria-labelledby": "basic-button",
         }}
       >
-        <MenuItem onClick={handleSchedule}>Schedule</MenuItem>
         <MenuItem onClick={handleLogout}>Log out</MenuItem>
       </Menu>
     </Fragment>
