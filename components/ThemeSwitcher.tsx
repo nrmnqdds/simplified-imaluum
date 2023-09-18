0dfbce6a93c2c8af078cc4d75c47458b0da71fce
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-19 00:53:52 +0800

    :rewind: Revert ThemeSwitcher component props

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,12 +1,23 @@
 "use client";
-
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 import { BsSun, BsMoon } from "react-icons/bs";
+import React from "react";
 
-export const ThemeSwitcher = ({ className = "", toggleTheme, theme }) => {
+export const ThemeSwitcher = ({ className = "" }) => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <button
       className={`w-fit p-2 rounded-md hover:scale-110 active:scale-100 duration-200 text-zinc-900 dark:text-slate-200 text-2xl font-bold ${className}`}
-      onClick={toggleTheme} // Use the toggleTheme function
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       {theme === "light" ? <BsMoon /> : <BsSun />}
     </button>
