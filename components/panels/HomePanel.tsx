5c27d4068e5a67f7a5100e8a6367e2f546b10d32
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-18 18:25:15 +0800

    :recycle: Uses better import aliases

--- components/panels/HomePanel.tsx
+++ components/panels/HomePanel.tsx
@@ -1,5 +1,6 @@
-import Disciplinary from "../Disciplinary";
-import Advertisement from "../Advertisements";
+import Disciplinary from "@components/Disciplinary";
+import Advertisement from "@components/Advertisements";
+import CGPA from "@components/CGPA";
 
 const HomePanel = () => {
   return (
@@ -9,7 +10,7 @@ const HomePanel = () => {
           <Disciplinary className={"border-zinc-500 border rounded-2xl"} />
           <Disciplinary className={"border-zinc-500 border rounded-2xl"} />
           <Disciplinary className={"border-zinc-500 border rounded-2xl"} />
-          <Disciplinary className={"border-zinc-500 border rounded-2xl"} />
+          <CGPA className={"border-zinc-500 border rounded-2xl"} />
         </div>
         <Advertisement className={"w-full h-[35%] flex flex-col"} />
       </div>
