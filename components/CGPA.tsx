ba2a1cc7f6931cee9ec6748ac750ac0ec30b6564
qryskalyst20 <quddusnuriman@gmail.com>
2023-09-18 18:23:25 +0800

    :construction: Creates CGPA component

---
+++ components/CGPA.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { useCookiesProvider } from "../app/context/cookies-provider";
+
+const CGPA = ({ className }) => {
+  const [loading, setLoading] = useState(true);
+  const { cookies } = useCookiesProvider();
+  const [sessionsList, setSessionsList] = useState({ sessionList: [] });
+  const [session, setSession] = useState("");
+  useEffect(() => {
+    async function getSession() {
+      const response = await fetch(`api/getCGPA`);
+
+      setLoading(false);
+    }
+    getSession();
+  }, [cookies]);
+
+  return (
+    <div className={`flex items-center justify-center ${className}`}>
+      {/* <h1>CGPA</h1> */}
+      <h1>Coming Soon</h1>
+    </div>
+  );
+};
+
+export default CGPA;
