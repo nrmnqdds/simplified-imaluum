3569ea95c28735764e8225d2056bd42d29eaa3c7
qryskalyst20 <quddusnuriman@gmail.com>
2023-10-06 22:54:24 +0000

    :lipstick: Fixes sidebar styling

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { Fragment } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Disclosure } from "@headlessui/react";
 import {
@@ -16,11 +16,13 @@ import { FaRegComments } from "react-icons/fa";
 import { BiMessageSquareDots } from "react-icons/bi";
 import { useRouter } from "next/navigation";
 import { ThemeSwitcher } from "./ThemeSwitcher";
+import Image from "next/image";
+import LOGO from "@/public/logo-landing-page.png";
 
 function SideNavbar() {
   const router = useRouter();
   return (
-    <div>
+    <Fragment>
       <Disclosure as="nav">
         <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-zinc-800  dark:text-slate-200 hover:bg-zinc-900 hover:text-slate-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-slate-100 group">
           <GiHamburgerMenu
@@ -29,10 +31,14 @@ function SideNavbar() {
           />
         </Disclosure.Button>
         <div className="p-6 w-1/2 h-screen bg-slate-200 dark:bg-zinc-950 z-20 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
-          <div className="flex flex-col justify-start item-center">
-            <h1 className="text-base text-center cursor-pointer font-bold text-blue-900 border-b border-zinc-800 dark:border-zinc-100 pb-4 w-full">
-              Virtual Dashboard
-            </h1>
+          <div className="flex flex-col justify-start items-start">
+            <Image
+              src={LOGO}
+              alt=""
+              width={20}
+              height={20}
+              className="object-contain dark:invert self-center"
+            />
             <div className=" my-4 border-b border-zinc-800 dark:border-zinc-100 pb-4">
               <div
                 onClick={() => router.replace("/dashboard")}
@@ -93,7 +99,7 @@ function SideNavbar() {
               </div>
             </div>
             {/* logout */}
-            <div className=" my-4">
+            <div className="my-4 w-full">
               <div className="flex mb-2 justify-start items-center gap-4 pl-5 border border-zinc-200  hover:bg-zinc-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
                 <MdOutlineLogout className="text-2xl text-zinc-600 group-hover:text-slate-200 " />
                 <h3 className="text-base text-zinc-800 dark:text-slate-200 group-hover:text-slate-200 font-semibold ">
@@ -101,13 +107,13 @@ function SideNavbar() {
                 </h3>
               </div>
             </div>
-            <div className="flex items-center justify-center">
-              <ThemeSwitcher />
+            <div className="self-center">
+              <ThemeSwitcher className="focus:outline-none" />
             </div>
           </div>
         </div>
       </Disclosure>
-    </div>
+    </Fragment>
   );
 }
 
