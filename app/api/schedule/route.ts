007f97640533e862295f2b0301df0b1e962a4b75
Qagura <65181897+qryskalyst20@users.noreply.github.com>
2023-10-08 23:51:37 +0800

    :fire: Update route.ts

--- app/api/schedule/route.ts
+++ app/api/schedule/route.ts
@@ -2,17 +2,10 @@ import axios, { AxiosResponse } from "axios";
 import { NextResponse } from "next/server";
 import { load } from "cheerio";
 import { cookies } from "next/headers";
-import { redis } from "@/lib/redis";
 
 export async function POST(request: Request) {
   const { session } = await request.json();
   // console.log("session", session);
-
-  const cachedValue = await redis.get(session);
-
-  if (cachedValue) {
-    return NextResponse.json({ schedule: JSON.parse(cachedValue) });
-  }
 
   const url = `https://imaluum.iium.edu.my/MyAcademic/schedule${session}`;
   console.log("url", url);
@@ -108,7 +101,7 @@ export async function POST(request: Request) {
   // const hiddenText = hiddenTextSelector.text().trim().replace(/\s+/g, " "); // Trim and replace multiple whitespace with a single space;
 
   // redis.set("schedule", JSON.stringify(schedule));
-  redis.set(session, JSON.stringify(schedule));
+
 
   // redis.set("schedule", JSON.stringify(schedule));
 
