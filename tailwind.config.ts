4416a33773d6aba2aed880daec2ab203dd1c81b4
qryskalyst20 <quddusnuriman@gmail.com>
2023-10-23 11:10:35 +0000

    :fire: Removes unnecessary config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,6 @@ module.exports = {
     "./app/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       dropShadow: {
         glow: [
@@ -22,26 +15,7 @@ module.exports = {
           "0 0px 30px rgba(255,255,255,0.9)",
         ],
       },
-      boxShadow: {
-        neumorphism: [
-          "-4px -4px 10px rgb(255, 255, 255), 4px 4px 10px rgba(0, 0, 0, 0.219)",
-        ],
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms")],
 };
