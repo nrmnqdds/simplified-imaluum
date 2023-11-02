1ffffbd0b25c33b6923efc8b84652353e03f9a4f
qryskalyst20 <quddusnuriman@gmail.com>
2023-11-02 21:38:49 +0000

    :bug: Extends time until 11pm

--- components/schedule/index.tsx
+++ components/schedule/index.tsx
@@ -3,25 +3,31 @@ import TimetableRow from "./TimetableRow";
 import moment from "moment";
 import TimetableEventBlock from "./TimetableEventBlock";
 import range from "@/utils/range";
+import { useState, Fragment } from "react";
+import TimetableModal from "./TimetableModal";
 
 interface TimetableProps {
-  events: TimetableEvent[];
+  events: Subject[];
 }
 
 export default function Timetable({ events }: TimetableProps) {
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [currentSubject, setCurrentSubject] = useState<Subject>();
+
   function getTimetableConfig(): TimetableConfig {
     let startDay = 0,
       endDay = 6,
       startHour = 8,
-      endHour = 17;
+      endHour = 23;
 
     for (const event of events) {
       // console.log("event", event);
-      if (event.weekTime.day < startDay) startDay = event.weekTime.day;
-      if (event.weekTime.day > endDay) endDay = event.weekTime.day;
-      const x = moment(event.weekTime.start, "HH:mm:ss").get("hours");
+      if (event.timestamps[0].day < startDay)
+        startDay = event.timestamps[0].day;
+      if (event.timestamps[0].day > endDay) endDay = event.timestamps[0].day;
+      const x = moment(event.timestamps[0].start, "HH:mm:ss").get("hours");
       if (x < startHour) startHour = x;
-      const y = moment(event.weekTime.end, "HH:mm:ss").get("hours");
+      const y = moment(event.timestamps[0].end, "HH:mm:ss").get("hours");
       if (y > endHour) endHour = y;
     }
 
@@ -42,29 +48,41 @@ export default function Timetable({ events }: TimetableProps) {
     return events;
   }
 
+  const handleModal = (event: Subject) => {
+    setCurrentSubject(event);
+    setOpenModal(!openModal);
+  };
+
   return (
-    <div className="relative h-full bg-transparent">
-      <TimetableWeekDays startDay={config.startDay} endDay={config.endDay} />
-      {range(config.startHour, config.endHour).map((hour, index) => (
-        <TimetableRow
-          key={index}
-          hour={hour}
-          showHour={index !== 0}
-          numberOfDays={config.endDay - config.startDay + 1}
-        />
-      ))}
-      <div className="absolute top-0 bottom-0 left-0 -right-px pl-14">
-        <div className="relative w-full h-full">
-          {getEvents().map((event, index) => (
-            <TimetableEventBlock
-              key={index}
-              event={event}
-              config={config}
-              onClick={event.onClick}
-            />
-          ))}
+    <Fragment>
+      <TimetableModal
+        currentSubject={currentSubject}
+        openModal={openModal}
+        setOpenModal={setOpenModal}
+      />
+      <div className="relative h-full bg-transparent">
+        <TimetableWeekDays startDay={config.startDay} endDay={config.endDay} />
+        {range(config.startHour, config.endHour).map((hour, index) => (
+          <TimetableRow
+            key={index}
+            hour={hour}
+            showHour={index !== 0}
+            numberOfDays={config.endDay - config.startDay + 1}
+          />
+        ))}
+        <div className="absolute top-0 bottom-0 left-0 -right-px pl-14">
+          <div className="relative w-full h-full">
+            {getEvents().map((event, index) => (
+              <TimetableEventBlock
+                key={index}
+                event={event}
+                onClick={handleModal}
+                config={config}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
