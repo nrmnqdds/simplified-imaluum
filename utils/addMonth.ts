2f1630a7f3e8592a9847d111384357d113b7b12a
qryskalyst20 <quddusnuriman@gmail.com>
2023-11-02 02:27:49 +0000

    :fire: Removes unnecessary files

--- utils/addMonth.ts
+++
@@ -1,6 +0,0 @@
-export default function addMonth(date: string, month: number): string {
-  const newDate = new Date(date);
-  newDate.setDate(1);
-  newDate.setMonth(newDate.getMonth() + month);
-  return newDate.toString();
-}
