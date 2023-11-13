068c77ba3c75ded2715df532d44cc9514d4637f9
qryskalyst20 <quddusnuriman@gmail.com>
2023-11-13 11:44:53 +0800

    :chart_with_upwards_trend: Umami integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import { ThemeProvider } from "./context/ThemeProvider";
+import Script from "next/script";
 import { Metadata } from "next";
 
 const poppins = Poppins({ subsets: ["latin"], weight: "400", display: "swap" });
@@ -17,6 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        src="https://analytics.eu.umami.is/script.js"
+        data-website-id="4f1a0eb5-2fe9-4578-badd-6777440a1f6a"
+      />
       <body className={poppins.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
