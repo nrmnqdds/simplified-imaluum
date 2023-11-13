0a64b0974835ba01b01a382f6ab3d54b9bd9962d
qryskalyst20 <quddusnuriman@gmail.com>
2023-11-13 15:30:40 +0800

    :sparkles: Displays more statistics for exam results

--- app/(home)/result/page.tsx
+++ app/(home)/result/page.tsx
@@ -9,7 +9,27 @@ import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
 const Page = () => {
   const { results } = ImaluumClient() || {};
   const [subjects, setSubjects] = useState([]);
+  const [session, setSession] = useState<String>("");
+  const [notes, setNotes] = useState({
+    remarks: "",
+    status: "",
+  });
   const [sortOrder, setSortOrder] = useState("asc"); // "asc" or "desc"
+
+  useEffect(() => {
+    if (session) {
+      const tempRemarks = results.filter(
+        (result) => result.sessionName === session
+      )[0].remarks;
+      const tempStatus = results.filter(
+        (result) => result.sessionName === session
+      )[0].status;
+      setNotes({
+        remarks: tempRemarks,
+        status: tempStatus,
+      });
+    }
+  }, [session, results]);
 
   const toggleSortOrder = () => {
     setSortOrder(sortOrder === "asc" ? "desc" : "asc");
@@ -20,68 +40,83 @@ const Page = () => {
     const isPassedA = isPassed(a.courseGrade);
     const isPassedB = isPassed(b.courseGrade);
 
-    // Determine sorting direction based on sortOrder
     const direction = sortOrder === "asc" ? 1 : -1;
 
-    // Sort by passed status first, and then alphabetically by course code
     if (isPassedA === isPassedB) {
       return direction * a.courseCode.localeCompare(b.courseCode);
     }
 
-    // Sort passed subjects first
     return direction * (isPassedA ? -1 : 1);
   });
 
+  const passedCount = subjects.reduce((count, subject) => {
+    return count + (isPassed(subject.courseGrade) ? 1 : 0);
+  }, 0);
+
+  const failedCount = subjects.length - passedCount;
+
   return (
-    <main className="flex-1 min-h-screen p-5">
-      <div className="flex flex-row items-center justify-center w-full h-[30vh] rounded-xl border-2 border-slate-400/10 bg-neutral-200 p-4 dark:bg-neutral-900">
-        <div className="flex-[2] w-full h-full">
-          <CGPA context={results} />
-        </div>
-        <div className="flex-1 flex flex-col gap-2 overflow-y-scroll scrollbar-hide">
-          {results?.map((result, index) => (
-            <div key={index} className="flex flex-col text-xs md:text-sm">
-              <p>
-                <span className="font-bold text-zinc-900 dark:text-slate-100">
-                  Session:{" "}
-                </span>
-                {result.sessionName}
-              </p>
-              <p>
-                <span className="font-bold text-zinc-900 dark:text-slate-100">
-                  GPA:{" "}
-                </span>
-                {result.gpaValue}
-              </p>
-              <p>
-                <span className="font-bold text-zinc-900 dark:text-slate-100">
-                  CGPA:{" "}
-                </span>
-                {result.cgpaValue}
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
+    <main className="flex-1 flex flex-col gap-2 min-h-screen px-2">
       {!results ? (
         <div>Loading...</div>
       ) : (
-        <ResultSwitcher courses={results} setEvents={setSubjects} />
+        <ResultSwitcher
+          courses={results}
+          setEvents={setSubjects}
+          setSession={setSession}
+        />
       )}
-      <div className="flex flex-col items-center justify-center w-full h-full rounded-xl border-2 border-slate-400/10 bg-neutral-200 p-4 dark:bg-neutral-900 mt-2">
+
+      <div className="flex flex-col lg:flex-row gap-5 items-center justify-center w-full h-full rounded-xl border-2 border-slate-400/10 bg-neutral-200 p-4 dark:bg-neutral-900">
+        <div className="flex-1 flex flex-row items-center justify-center gap-5">
+          <div className="flex flex-col gap-2 items-center justify-center bg-neutral-300 p-4 font-semibold dark:bg-neutral-800 border-zinc-700 rounded-xl border">
+            <h1 className="text-zinc-300 text-xs lg:text-sm">
+              Subjects Taken:
+            </h1>
+            <p className="text-zinc-100 text-lg lg:text-3xl">
+              {subjects.length}
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 items-center justify-center rounded-xl bg-cyan-500/10 p-4 text-sm font-semibold leading-6 text-cyan-600 dark:text-cyan-400 ring-1 ring-inset ring-cyan-500/20">
+            <h1 className="text-xs lg:text-sm">Subjects Passed:</h1>
+            <p className="text-lg lg:text-3xl">{passedCount}</p>
+          </div>
+          <div className="flex flex-col gap-2 items-center justify-center rounded-xl bg-red-500/10 p-4 text-sm font-semibold leading-6 text-red-600 dark:text-red-400 ring-1 ring-inset ring-red-500/20">
+            <h1 className="text-xs lg:text-sm">Subjects Failed:</h1>
+            <p className="text-lg lg:text-3xl">{failedCount}</p>
+          </div>
+        </div>
+        <div className="flex-1 flex flex-row items-center justify-center gap-5">
+          <div className="flex flex-col gap-2 items-center justify-center bg-neutral-300 p-4 font-semibold dark:bg-neutral-800 border-zinc-700 rounded-xl border">
+            <h1 className="text-zinc-300 text-xs lg:text-sm">Status:</h1>
+            <p className="text-zinc-100 text-sm lg:text-xl">{notes.status}</p>
+          </div>
+          <div className="flex flex-col gap-2 items-center justify-center bg-neutral-300 p-4 font-semibold dark:bg-neutral-800 border-zinc-700 rounded-xl border">
+            <h1 className="text-zinc-300 text-xs lg:text-sm">Remarks:</h1>
+            <p className="text-zinc-100 text-sm lg:text-xl">{notes.remarks}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full h-full rounded-xl border-2 border-slate-400/10 bg-neutral-200 p-4 dark:bg-neutral-900">
         <div id="header" className="w-full flex flex-row mb-2 p-4">
-          <div className="flex-1">
-            <p className="zinc-100">Course Code</p>
+          <div className="flex-1 w-fit flex items-center justify-start text-xs sm:text-sm">
+            <p className="zinc-100 lg:pl-24">Subject Code</p>
           </div>
-          <div className="flex-1">
-            <p className="zinc-100">Grade</p>
+          <div className="flex-1 hidden sm:flex items-center justify-center text-center lg:justify-start text-xs sm:text-sm">
+            <p className="zinc-100">Subject Name</p>
           </div>
-          <div className="flex-1 flex flex-row">
-            <span className="cursor-pointer flex items-center pr-2">
+          <div className="flex-1 flex items-center justify-center lg:justify-start text-xs sm:text-sm">
+            <p className="zinc-100 lg:pl-24">Grade</p>
+          </div>
+          <div
+            className="flex-1 flex flex-row items-center justify-start cursor-pointer text-xs sm:text-sm"
+            onClick={toggleSortOrder}
+          >
+            <span className="flex items-center pr-2">
               <ChevronUpDownIcon
                 className="h-5 w-5 text-slate-100"
                 aria-hidden="true"
-                onClick={toggleSortOrder}
               />
             </span>
             <p className="zinc-100">Status</p>
@@ -95,19 +130,22 @@ const Page = () => {
               id="subjects"
               className="w-full flex flex-row bg-neutral-300 p-4 dark:bg-neutral-800 border-zinc-700 rounded-xl border"
             >
-              <div className="flex-1">
-                <p className="zinc-100">{subject.courseCode}</p>
+              <div className="flex-1 w-fit flex items-center justify-start text-xs sm:text-sm">
+                <p className="zinc-100 lg:pl-24">{subject.courseCode}</p>
               </div>
-              <div className="flex-1">
-                <p className="zinc-100">{subject.courseGrade}</p>
+              <div className="flex-1 hidden sm:flex items-center justify-start text-xs sm:text-sm">
+                <p className="zinc-100">{subject.courseName}</p>
               </div>
-              <div className="flex-1">
+              <div className="flex-1 flex items-center justify-center lg:justify-start text-xs sm:text-sm">
+                <p className="zinc-100 lg:pl-24">{subject.courseGrade}</p>
+              </div>
+              <div className="flex-1 flex items-center justify-start text-xs sm:text-sm">
                 {isPassed(subject.courseGrade) ? (
-                  <p className="w-fit rounded-full bg-cyan-500/10 px-3 py-1 text-sm font-semibold leading-6 text-cyan-600 dark:text-cyan-400 ring-1 ring-inset ring-indigo-500/20">
+                  <p className="w-fit rounded-full bg-cyan-500/10 px-3 py-1 font-semibold text-cyan-600 dark:text-cyan-400 ring-1 ring-inset ring-cyan-500/20">
                     Passed
                   </p>
                 ) : (
-                  <p className="w-fit rounded-full bg-red-500/10 px-3 py-1 text-sm font-semibold leading-6 text-red-600 dark:text-red-400 ring-1 ring-inset ring-indigo-500/20">
+                  <p className="w-fit rounded-full bg-red-500/10 px-3 py-1 font-semibold text-red-600 dark:text-red-400 ring-1 ring-inset ring-red-500/20">
                     Failed
                   </p>
                 )}
