bc900da5093c6b54cc2a33838c4a76573b2253c5
qryskalyst20 <quddusnuriman@gmail.com>
2023-12-01 02:35:07 +0800

    :fire: Removes unnecessary lines

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -311,7 +311,20 @@ export default function HomeLayout({
             </div>
           </div>
 
-          <main className="lg:overflow-x-hidden">{children}</main>
+          <main className="lg:overflow-x-hidden">
+            <Transition
+              appear={true}
+              show={true}
+              enter="transition-opacity duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity duration-150"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              {children}
+            </Transition>
+          </main>
         </div>
       </div>
     </ImaluumProvider>
