6854adc057fd5fd7c08989367127242e8f08d955
nrmnqdds <quddusnuriman@gmail.com>
2023-12-26 21:56:28 +0800

    :recycle: uses zustand to pass state across components

--- app/(home)/dashboard/page.tsx
+++ app/(home)/dashboard/page.tsx
@@ -2,17 +2,25 @@
 
 import Advertisement from "@/components/Advertisements";
 import CGPA from "@/components/CGPA";
-import ImaluumClient from "@/lib/imaluumClient";
-import { Fragment } from "react";
+import useResult from "@/hooks/useResult";
+import { useEffect } from "react";
 
-const Page = () => {
-  const { results } = ImaluumClient() || {};
+const DashboardPage = () => {
+  const { result } = useResult();
+
+  useEffect(() => {
+    if (result) {
+      console.log(result);
+    } else {
+      console.log("no result");
+    }
+  }, [result]);
 
   return (
     <div className="min-h-screen flex flex-col py-10 px-4 sm:px-6 lg:px-8">
       <div className="grid auto-rows-[192px] grid-cols-3 gap-2">
-        {!results ? (
-          <Fragment>
+        {!result ? (
+          <>
             {[...Array(7)].map((_, i) => (
               <div
                 key={i}
@@ -21,9 +29,9 @@ const Page = () => {
                 }`}
               />
             ))}
-          </Fragment>
+          </>
         ) : (
-          <Fragment>
+          <>
             <div className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-200 p-4 dark:bg-neutral-900">
               Coming Soon
             </div>
@@ -34,11 +42,11 @@ const Page = () => {
               Coming Soon
             </div>
             <div className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-200 p-4 dark:bg-neutral-900 col-span-2">
-              <CGPA context={results} />
+              <CGPA context={result} />
               {/* Coming Soon */}
             </div>
             <div className="flex flex-col gap-2 row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-200 p-4 dark:bg-neutral-900 overflow-y-scroll scrollbar-hide">
-              {results?.map((result, index) => {
+              {result?.map((result, index) => {
                 return (
                   <div key={index} className="flex flex-col text-xs md:text-sm">
                     <p>
@@ -69,7 +77,7 @@ const Page = () => {
             <div className="row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-200 p-4 dark:bg-neutral-900 col-span-2">
               Coming Soon
             </div>
-          </Fragment>
+          </>
         )}
       </div>
 
@@ -78,4 +86,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default DashboardPage;
