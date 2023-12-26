32ce468f962fb55d67907309703f44d86b825c27
nrmnqdds <quddusnuriman@gmail.com>
2023-12-26 22:04:16 +0800

    :rotating_light: biomejs linting

--- context/QueryProvider.tsx
+++ context/QueryProvider.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-// Create a client
 const queryClient = new QueryClient();
 
 export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    // Provide the client to your App
-    <QueryClientProvider client={queryClient}>{children}<ReactQueryDevtools initialIsOpen={false} /></QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
