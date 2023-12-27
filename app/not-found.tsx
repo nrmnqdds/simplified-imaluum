5d6494ec1268fed25ca6cbf846b62bb1ebc1866e
nrmnqdds <quddusnuriman@gmail.com>
2023-12-27 11:53:05 +0800

    :lipstick: change lottie for 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,10 +5,16 @@ import LottiePlayer from "@/components/LottiePlayer";
 
 const NotFound = () => {
   return (
-    <main className="bg-zinc-900 w-full h-screen flex items-center justify-center">
-      <LottiePlayer
-        animationData={require("../public/lottie/Animation - 1703013994707.lottie")}
-      />
+    <main className="bg-zinc-900 w-full h-screen flex flex-col items-center justify-center p-24">
+      <div className="px-24">
+        <LottiePlayer
+          animationData={require("../public/lottie/notfound.lottie")}
+        />
+      </div>
+      <h1 className="text-white text-3xl font-bold">
+        Error: <span className="text-red-500">404</span>
+      </h1>
+      <h1 className="text-white text-3xl font-bold">Not found</h1>
     </main>
   );
 };
