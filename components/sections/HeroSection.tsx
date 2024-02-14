d1b0b548a7c348ec187b701566312f1d8a1ff651
nrmnqdds <quddusnuriman@gmail.com>
2024-02-14 23:39:23 +0800

    :wheelchair: login form conditional rendering

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,11 +1,13 @@
+import LoginForm from "@/components/LoginForm";
 import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+import { Button } from "@/components/ui/button";
 import LIGHTDEMOPIC from "@/public/imaluum.nrmnqdds.com_schedule (1).png";
 import DARKDEMOPIC from "@/public/imaluum.nrmnqdds.com_schedule.png";
 import LOGO from "@/public/logo-landing-page.png";
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import { cookies } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
-import LoginForm from "../LoginForm";
 
 const Hero = () => {
   return (
@@ -90,7 +92,13 @@ const Hero = () => {
             make i-Ma&apos;luum more user-friendly.
           </p>
 
-          <LoginForm />
+          {cookies().get("imaluum-session") ? (
+            <Link href="/dashboard">
+              <Button className="mt-10">Go to dashboard</Button>
+            </Link>
+          ) : (
+            <LoginForm />
+          )}
         </div>
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
