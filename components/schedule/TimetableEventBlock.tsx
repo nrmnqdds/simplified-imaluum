4d2a75cf8a7b5656be4f5c051e95142dcc6abf32
nrmnqdds <quddusnuriman@gmail.com>
2024-02-22 19:11:42 +0800

    :bug: fix tailwind colors

--- components/schedule/TimetableEventBlock.tsx
+++ components/schedule/TimetableEventBlock.tsx
@@ -1,6 +1,20 @@
 import { cn } from "@/lib/common/cn";
 import moment from "moment";
 
+const predefinedColors = [
+  "bg-red-200 text-red-700 border-red-500 hover:bg-red-300 hover:text-red-800",
+  "bg-sky-200 text-sky-700 border-sky-500 hover:bg-sky-300 hover:text-sky-800",
+  "bg-fuchsia-200 text-fuchsia-700 border-fuchsia-500 hover:bg-fuchsia-300 hover:text-fuchsia-800",
+  "bg-orange-200 text-orange-700 border-orange-500 hover:bg-orange-300 hover:text-orange-800",
+  "bg-lime-200 text-lime-700 border-lime-500 hover:bg-lime-300 hover:text-lime-800",
+  "bg-yellow-200 text-yellow-700 border-yellow-500 hover:bg-yellow-300 hover:text-yellow-800",
+  "bg-emerald-200 text-emerald-700 border-emerald-500 hover:bg-emerald-300 hover:text-emerald-800",
+  "bg-pink-200 text-pink-700 border-pink-500 hover:bg-pink-300 hover:text-pink-800",
+  "bg-indigo-200 text-indigo-700 border-indigo-500 hover:bg-indigo-300 hover:text-indigo-800",
+  "bg-stone-200 text-stone-700 border-stone-500 hover:bg-stone-300 hover:text-stone-800",
+  "bg-purple-200 text-purple-700 border-purple-500 hover:bg-purple-300 hover:text-purple-800",
+];
+
 interface TimetableEventBlockProps {
   event: Subject;
   config: TimetableConfig;
@@ -42,7 +56,7 @@ export default function TimetableEventBlock({
       type="button"
       className={cn(
         "absolute rounded-md duration-100 p-1 border md:border-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
-        event.color
+        [event.color]
       )}
       style={{
         top,
